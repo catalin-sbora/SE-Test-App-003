e1cb0fab7cfc1ff83dc08c46140cab07fbf8d0dd
catalin-sbora <catalin.sbora@gmail.com>
2025-03-25 10:59:36 +0200

    Update Program.cs

--- TestApplication/Program.cs
+++ TestApplication/Program.cs
@@ -5,9 +5,10 @@ namespace TestApplication
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-asdadasd asdajkdsja skdasda;
+
             // Add services to the container.
-            builder.Services.AddRazorPages();
+            builder.Services
+                .AddRazorPages();
 
             var app = builder.Build();
 
